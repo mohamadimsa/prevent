c4593a6187c39796178607cf310ec51ccb36bd7d
mohamed <mohamadi1.msa-abdou@epitech.eu>
2021-09-18 15:30:05 +0200

    ajout boutton et logique inscription

--- client/src/screen/InscriptionScreen.js
+++ client/src/screen/InscriptionScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View , Text} from "react-native";
+import { View , Text, Button} from "react-native";
+import { useAuth } from "../api/auth/useAuth";
+
 
 const InscriptionScript = ()=>{
+    const {inscription} = useAuth()
     return(
         <View>
             <Text>inscription</Text>
+            <Button title="s'inscrire" onPress={()=>{inscription('houda1234','tnifasshouda@gmail.com','houda123')}}/>
         </View>
     )
 }
