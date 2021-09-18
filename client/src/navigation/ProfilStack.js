fa493074bbff91abce6370e03550677341fc9b3b
mohamed <mohamadi1.msa-abdou@epitech.eu>
2021-09-18 21:56:08 +0200

    amelioration navigation

--- client/src/navigation/ProfilStack.js
+++ client/src/navigation/ProfilStack.js
@@ -5,7 +5,7 @@ import ProfilScreen from "../screen/ProfilScreen";
 import MonCompteScreen from "../screen/MonCompteScreen"
 const ProfilStackScreen = () => {
   return (
-    <ProfilStack.Navigator>
+    <ProfilStack.Navigator  screenOptions={{headerShown: false}}>
       <ProfilStack.Screen name="profil" component={ProfilScreen} />
       <ProfilStack.Screen name="mon compte" component={MonCompteScreen} />
     </ProfilStack.Navigator>
--- client/src/navigation/menuTab.js
+++ client/src/navigation/menuTab.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {createBottomTabNavigator } from'@react-navigation/bottom-tabs';
 const Tabs = createBottomTabNavigator();
 import HomeScreen from '../screen/HomeScreen';
-import SearchStackScreen from './SerieStack'
+import SearchStackScreen from '../screen/SearchScreen'
 
 
 const TabsScreen = () => (
-    <Tabs.Navigator>
+    <Tabs.Navigator  screenOptions={{headerShown: false}}>
       <Tabs.Screen name="Home" component={HomeScreen} />
       <Tabs.Screen name="Search" component={SearchStackScreen} />
     </Tabs.Navigator>
