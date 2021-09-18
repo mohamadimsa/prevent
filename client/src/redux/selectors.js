6ff5e86b417be8061264fd7f4e5ac2f93cc52de7
mohamed <mohamadi1.msa-abdou@epitech.eu>
2021-09-18 14:19:22 +0200

    recorection

--- client/src/redux/selectors.js
+++ client/src/redux/selectors.js
@@ -1,2 +1,2 @@
-export const getUser = store => store.auth.error;
+export const error = store => store.auth.error;
 export const isAUth  = store => store.auth.authenticate;
--- client/src/screen/ConnectionScreen.js
+++ client/src/screen/ConnectionScreen.js
@@ -1,21 +1,20 @@
 import React ,{useEffect}from "react";
 import { View , Text, Button} from "react-native";
 import { useSelector } from "react-redux";
-import{getUser,isAUth} from "../../src/redux/selectors";
+import{error,isAUth} from "../../src/redux/selectors";
 import { useAuth } from "../api/auth/useAuth";
 
 const ConnectionScreen = ({navigation})=>{
-   const error = useSelector(getUser)
+   const e = useSelector(error)
    const Auth = useSelector(isAUth)
    const {authentification} = useAuth();
    console.log('l utilisateur est authentifier : '+Auth);
-   useEffect(() => {
-     authentification('petubrt@gmail.com','mohamadiMsa');
-   }, [])
+   
+
     return(
         <View>
             <Text>screen connection moh</Text>
-            <Button title='cree un compte' onPress={()=>navigation.push("inscription")}></Button>
+            <Button title='se connecter' onPress={()=>authentification('petubrt@gmail.com','mohamadiMsa')}></Button>
         </View>
     )
 }
