e0c64626460c0e80e6739c78d80eb2c0b79e04e0
mohamed <mohamadi1.msa-abdou@epitech.eu>
2021-09-19 15:36:48 +0200

    a

--- client/src/api/serie/useSerie.js
+++ client/src/api/serie/useSerie.js
@@ -2,13 +2,13 @@ import { useDispatch } from "react-redux";
 import { all_series } from "./../../redux/actions/index";
 import axios from "axios";
 import { url, key } from "../config";
-import { useSelector } from "react-redux";
-import { token } from "../../src/redux/selectors";
+/* import { useSelector } from "react-redux";
+import { token } from "../../src/redux/selectors"; */
 
 
 export const useSerie = () => {
     const dispatch = useDispatch();
-    const tok = useSelector(token)
+    /* const tok = useSelector(token) */
     const header = {
         "Content-Type": "multipart/form-data"
     }
@@ -21,9 +21,10 @@ export const useSerie = () => {
                 console.log(e);
             })
     }
-    const addSerie = async (id) => {
+  /*   const addSerie = async (id) => {
         const responce = await axios.get(`${url}/shows/show?id=${id}&${tok}`)
-    }
+        
+    } */
 
     return {
         getAll
